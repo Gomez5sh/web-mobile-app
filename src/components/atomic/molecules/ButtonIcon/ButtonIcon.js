7ff0484b291472f64c3ff3b3533689ad3f046e08
gomez5sh <emmanuel.gomez@vertebrasoluciones.com>
2021-08-30 08:14:17 -0500

    [ADD]: News

--- src/components/atomic/molecules/ButtonIcon/ButtonIcon.js
+++ src/components/atomic/molecules/ButtonIcon/ButtonIcon.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "../../atoms/Button/index";
+import Icon from "../../atoms/Icon/index";
 
-
-const ButtonIcon = ({children, type, icon}) =>(
-    <Button type="secondary">{children}<Icon type="right-arrow" /></Button>
-)
+const ButtonIcon = ({ children, type, icon }) => (
+  <Button type="secondary">
+    {children}
+    <Icon type="right-arrow" />
+  </Button>
+);
 
 ButtonIcon.propTypes = {
-    children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired,
+};
 
-export default ButtonIcon
+export default ButtonIcon;
